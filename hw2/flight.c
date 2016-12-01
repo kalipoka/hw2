e5ab6dfc9dd7371f89199212d8ddfd8e8cfc0da3
Alex <saraevalex@gmail.com>
2016-12-01 16:33:41 +0200

    add destination size global limits

--- hw2/flight.c
+++ hw2/flight.c
@@ -1,1 +1,5 @@
 /*hello alex*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "flight.h"#include "ex2.h"static PFLIGHT g_ArrElements[MAX_ID] = { 0 };  /* array of flights*/
