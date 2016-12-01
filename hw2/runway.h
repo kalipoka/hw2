c2d9d93f506f9de6ee375c277d7f6003ef443d2f
ofir <ofiris92@gmail.com>
2016-12-01 16:36:50 +0200

    adding runway.h and runway.c

---
+++ hw2/runway.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "ex2.h"
+
+
+//runway_num
+//type
+//list_of_flights - connected list one way 
+
+
+
+int* create_runway(char flight_id[3], FlightType flight_type);
+{
+
+
+
+	// return an id to the created runway
+}
+
+
+void destroyRunway(in)
+/* destroy the runway and waiting list in the runway*/
+
+
+BOOL isFlightExists(in)
